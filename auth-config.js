c8aacfcfce2b1323fd24a335e258d4eedccd5285
Mofil <mofila@amazon.com>
2025-12-16 11:53:36 +0530

    Add 'ningwa' to authorized users list

--- auth-config.js
+++ auth-config.js
@@ -1,6 +1,7 @@
 // auth-config.js
 window.AUTH_CONFIG = {
-    authorizedUsers: ["mofila", "bhaver", "brauera", "ahrensh", "qian", "danyangl", "buenavef", "dcchiluk", "khandavp"]
+    authorizedUsers: ["mofila", "bhaver", "brauera", "ahrensh", "qian", "danyangl", "ningwa", "buenavef", "dcchiluk", "khandavp"]
 };
+
 
 
