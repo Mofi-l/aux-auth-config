cc130e68aee6f1052ee2d467425400074c938acd
Mofil <mofila@amazon.com>
2026-02-28 21:02:01 +0530

    Update auth-config.js

--- auth-config.js
+++ auth-config.js
@@ -1,7 +1,8 @@
 // auth-config.js
 window.AUTH_CONFIG = {
-    authorizedUsers: ["bhaver", "kantaspu", "brauera", "ahrensh", "qian", "danyangl", "ningwa", "buenavef", "dcchiluk", "khandavp"]
+    authorizedUsers: ["mofila", "bhaver", "kantaspu", "brauera", "ahrensh", "qian", "danyangl", "ningwa", "buenavef", "dcchiluk", "khandavp"]
 };
+
 
 
 
